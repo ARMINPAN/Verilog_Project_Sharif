3f547efbb42b88c7e61b5454804f13f8e88972b1
Your Name <arminpp1379@gmail.com>
2020-12-19 20:30:11 +0330

    debug

--- Convolutional encoder.v
+++ Convolutional encoder.v
@@ -1,22 +1,22 @@
 module Convolutional_Encoder(in,parities,CLK);
     input wire in;
-    input wire clock;
-    output wire parities[1:0];
-    wire memory[2:0];
-    reg g1[2:0] = 3'b110;
-    reg g2[2:0] = 3'b111;
+    input wire CLK;
+    output reg parities[1:0];
+    reg memmoryCells[2:0];
+    reg [2:0]g1 = 3'b110;
+    reg [2:0]g2 = 3'b111;
 
 
-    shiftRegister3Bit storage(.in(in), .out(memory), .CLK(CLK));
-    assign parrities[0] = g1[0] * memory[2] + g1[1] * memory[1] + g1[2] * memory[0];
-    assign parrities[1] = g2[0] * memory[2] + g2[1] * memory[1] + g2[2] * memory[0];
+    shiftRegister3Bit storage(.in(in), .out(memmoryCells), .CLK(CLK));
+    assign parities[0] = (g1[0] * memmoryCells[2] + g1[1] * memmoryCells[1] + g1[2] * memmoryCells[0])%2;
+    assign parities[1] = (g2[0] * memmoryCells[2] + g2[1] * memmoryCells[1] + g2[2] * memmoryCells[0])%2;
 endmodule
 
 
 ////////////////shiftregister
 module shiftRegister3Bit(in,out,CLK);
     input wire in,CLK;
-    output reg out[2:0];
+    output reg [2:0]out;
 
     always@(posedge CLK)
         begin
