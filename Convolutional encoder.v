bf740bac1b9d44c8760c882a09e7cb1332816c62
apourghani <66559378+apourghani@users.noreply.github.com>
2021-01-01 00:46:13 +0330

    Update Convolutional encoder.v

--- Convolutional encoder.v
+++ Convolutional encoder.v
@@ -2,27 +2,52 @@
 module Convolutional_Encoder(in,parities,CLK);
     input wire in;
     input wire CLK;
-    output wire [1:0]parities;
+    output reg [1:0]parities;
     wire [2:0]memmoryCells;
-    reg [2:0]g1 = 3'b110;
-    reg [2:0]g2 = 3'b111;
-
-
-    shiftRegister3Bit storage(.in(in), .out(memmoryCells), .CLK(CLK));
-    assign parities[0] = (g1[0] * memmoryCells[2] + g1[1] * memmoryCells[1] + g1[2] * memmoryCells[0])%2;
-    assign parities[1] = (g2[0] * memmoryCells[2] + g2[1] * memmoryCells[1] + g2[2] * memmoryCells[0])%2;
-endmodule
-
-
-//a shift register for saving constraint bits (here K=3)
-
-module shiftRegister3Bit(in,out,CLK);
-    input wire in,CLK;
-    output reg [2:0]out = 3'b0;
-
-    always@(posedge CLK)
-        begin
-            out[0] <= in;
-            {out[2],out[1]} <= {out[1],out[0]};     
-        end
+	 reg [1:0]state=2'b00; 
+	 
+	 
+	always@(posedge CLK)
+	begin
+				if(in == 1'b0 && state == 2'b00) 
+                begin
+                    parities = 2'b00;
+						  state = 2'b00;
+                end
+            else if(in == 1'b1 && state == 2'b00)
+                begin
+                    parities = 2'b11;
+                    state = 2'b01;
+                end
+            else if(in == 1'b0 && state == 2'b01)
+                begin
+                    parities = 2'b10;
+                    state = 2'b10;
+                end
+            else if(in == 1'b1 && state == 2'b01)
+                begin
+                    parities = 2'b01;
+                    state = 2'b11;
+                end
+            else if(in == 1'b0 && state == 2'b11)
+                begin
+                    parities = 2'b01;
+                    state = 2'b10;
+                end
+            else if(in == 1'b1 && state == 2'b11)
+                begin
+                    parities = 2'b10;
+                    state = 2'b11;
+                end
+            else if(in == 1'b0 && state == 2'b10)
+                begin
+                    parities = 2'b11;
+                    state = 2'b00;
+                end
+            else if(in == 1'b1 && state == 2'b10)
+                begin
+                    parities = 2'b00;
+                    state = 2'b01;
+                end
+	end
 endmodule
