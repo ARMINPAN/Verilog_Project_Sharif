b56ce6b0ca04b29adf47d262f57a9115bd693c95
Your Name <arminpp1379@gmail.com>
2020-12-31 23:53:41 +0330

    test bench added

--- System.v
+++ System.v
@@ -9,5 +9,34 @@ module System(in,CLK,out);
     Convolutional_Encoder c_enc(.in(in), .parities(coded_data), .CLK(CLK));
     Viterbi_V1 v_dec(.parities(coded_data), .CLK(CLK), .out(out));
 
-    
+
+endmodule
+
+
+
+module System_test();
+	// Inputs
+	reg in;
+	reg CLK;
+	// Outputs
+	wire out;
+
+	// Instantiate the Unit Under Test (UUT)
+	System uut (
+		.in(in),
+		.CLK(CLK),		
+		.out(out)
+	);
+
+	initial begin
+		// Initialize Inputs
+		in = 0;
+		CLK = 0;
+	end
+	always CLK=#10 ~CLK;
+	always@(posedge CLK)
+	begin
+	in= $random();
+	end
+
 endmodule
